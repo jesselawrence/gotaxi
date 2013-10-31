613171ee46ed68d995a71c5d83460ab1918ba7dd
wbmc <wbmc@wbmc.(none)>
2013-10-31 16:51:24 +0800

    v0.1

--- mapper.py
+++ mapper.py
@@ -3,15 +3,33 @@
 
 import sys
 
+from math import sin as sin
+from math import cos as cos
+from math import acos as acos
+
+#GPS Distance calculate
+def disGps(x1,y1,x2,y2):
+    pi = 3.1415
+    R = 6371.004
+    c = sin(y1)*sin(y2)*cos(x1-x2)+cos(y1)*cos(y2)
+    result = R * acos(c) * pi / 180
+    return result
+
+
+#di
+di = 0.0008
+
+
 #基础时间
 timebase = 2.01211e+13
+#datainput
+data = sys.stdin
 
-#标志位 是否进行路段输入
-flag = 0
-
-if flag == 0:
-    #路段数据输入
-    a = open('road.txt','r')
+#路段数据输入
+try :
+    type(eval('roadList'))
+except:
+    a = open('road1.txt','r')
     #生成路段列表
     roadList = list()
 
@@ -20,9 +38,12 @@ if flag == 0:
         i = eval(i)
         roadList.append(i)
 
-data = sys.stdin
+    a.close()
+else :
+    temp = 1    
 
 car = {}
+
 
 for i in data:
     try:
@@ -45,21 +66,50 @@ for i in data:
     x = i[4]
     y = i[5]
     timec = int(i[3])
-    minid = 0
-    for k in roadList:
-        if ((x - 0.0031) <= k[1] and (x + 0.0031) >= k[3] and (y - 0.0031) <= k[2] and (y + 0.0031) >= k[4]):
-            minid = int(k[0])
+    minid = []
+    
+    mid = len(roadList)/2
+    x1 = x + di
+    x2 = x - di
+    y1 = y + di
+    y2 = y - di
+    p1 = 0
+    p2 = len(roadList)
+    
+    while (roadList[p1][1] < x1):
+        mid = (p1 + p2) / 2
+        if roadList[mid][1] < x2:
+            p1 = mid
+        else:
             break
+    
+
+    for k in roadList:
+        if (x1 >= k[1]):
+            if (x2 <= k[3]):
+                if (y1 >= k[2]): 
+                    if (y2 <= k[4]):
+                        minid.append(int(k[0]))   
+    temp_min = 9999
+    if len(minid) > 0:
+        temp_minid = minid[0]
+        for k in minid:
+            temp = disGps(x, y, roadList[k-1][5], roadList[k-1][6])
+            if temp < temp_min:
+                temp_min = temp
+                temp_minid = k
+    else:
+        temp_minid = 0
 
     if (car.has_key(i[0])) :
-        if car[i[0]].count([timec, minid,i[2]]) == 0:
-            car[i[0]].append([timec, minid, i[2]])        #判断是否有重复计算的情况
-            print '%s %s %s' % (timec, minid, i[2])
+        if car[i[0]].count([timec, temp_minid,i[2]]) == 0 :
+            car[i[0]].append([timec, temp_minid, i[2]])        #判断是否有重复计算的情况
+            print '%s %s %s' % (timec, temp_minid, i[2])
         else :
             continue
 
     else :
-        car[i[0]]=list([timec,minid,i[2]])
-        print '%s %s %s' % (timec, minid, i[2])
+        car[i[0]]=list([timec,temp_minid,i[2]])
+        print '%s %s %s' % (timec, temp_minid, i[2])
 
 
